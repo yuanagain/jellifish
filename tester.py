e255a9cf149da5cdbe24cb97ec5997002a0a8d7e
Hxs123 <prestige.hxs@gmail.com>
2015-12-20 23:51:47 -0500

    Edited the tester.py file to make a list of tasknodes and create a CoreEngine object to get the optimal sequence

--- tester.py
+++ tester.py
@@ -20,21 +20,35 @@ from databasecaller import DatabaseCaller
 from databasecaller import Recipe as rp
 from TaskNode import TaskNode
 
+from algo.TnodeUtils import printTasks
+from algo.CoreEngine import CoreEngine as CE
+
+
 #print the status (and the status of its dependencies,
 #and their dependencies recursively)
 #for a given tasknode
-def printStatus(tasknode):
+def printStatus(tasknode, list_of_tasks):
 	tasknode.display()
 	for i in tasknode.dependencies:
-		printStatus(i)
+		printStatus(i, list_of_tasks)
+		#tnode = TaskNode(i.name, i.short_descr, i.long_descr, i.time)
+		list_of_tasks.append(i)
 
 def main():
+	list_of_tasks = []
 	db_caller = DatabaseCaller()
-	pulled_node = db_caller.create_tasknode('pot of water')
+	pulled_node = db_caller.create_tasknode('ramen')
 	if (pulled_node is None):
 		pass
 	else:
-		printStatus(pulled_node)
+		printStatus(pulled_node, list_of_tasks)
+		#list_of_tasks.append(pulled_node)
+	# list_of_tasks = db_caller.my_set
+	# print(db_caller.my_set)
+	# for i in list_of_tasks:
+	# 	print(i)
+	core_engine = CE(list_of_tasks)
+	core_engine.printTasks()
 
 if __name__ == '__main__':
     main()
