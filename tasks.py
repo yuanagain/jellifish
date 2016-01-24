2b1eb75edf5993e3c15934f994daa291d4530c01
Rushy Panchal <panchal.rushy@gmail.com>
2016-01-24 18:42:46 -0500

    Add comments to tasks.py and clean up certain tasks
    The comments and documentation better describe how the tasks work and are used.
    
    In addition, creating the string of dependencies to include/exclude in build_react
    was optimized by removing unnecessary string concatentation.
    
    Similarly, some code was shortened from build_sass by moving the actual "scss"
    shell call to another function, thus minimizing repeated code.

--- tasks.py
+++ tasks.py
@@ -1,6 +1,6 @@
 # Rushy Panchal
 # jellifish
-# fabfile.py
+# tasks.py
 
 from invoke import task, run
 
@@ -14,13 +14,21 @@ STATIC_PATH = os.path.join(os.getcwd(), "static")
 
 @task(name = "build")
 def build(external = False):
-	'''Run all build tasks'''
+	'''Run all build tasks
+
+	Arguments
+		bool external - whether or not to compile external dependencies
+	'''
 	build_react(external)
 	build_sass(external)
 
 @task(name = "build-react")
 def build_react(external = False):
-	'''Build ReactJS files into a single file'''
+	'''Build ReactJS files into a single file
+
+	Arguments
+		bool external - whether or not to (separately) compile external dependencies
+	'''
 	# get dependencies from Node
 	dependencies = run("""node -e 'var e=require("./package.json"); console.log(Object.keys(e.dependencies).join(" "));'""")
 	if not dependencies.ok:
@@ -28,20 +36,35 @@ def build_react(external = False):
 	else:
 		dependencies = dependencies.stdout.strip("\n").split(" ")
 
+	# bundle custom and dependency files separately
 	browserify("main.js", "bundle.js", dependencies)
-	if external:
+	if external: # there is no main source file for bundling the external files
 		browserify("", "external.js", dependencies)
 
 def browserify(in_path, out_path, deps):
-	'''Browserify a file'''
+	'''Browserify a file
+
+	Arguments
+		str in_path - source input of files to bundle
+		str out_path - output path of bundle
+		[str] deps - list of dependency names
+	'''
 	input_path = os.path.join(STATIC_PATH, "js", in_path)
 	output_path = os.path.join(STATIC_PATH, "js", out_path)
 
 	if in_path: # only bundle local files
 		in_stream = input_path
-		additional = "--no-bundle-external " + " ".join(map(lambda s: "-x " + s, deps))
+		if deps: # exclude external dependencies from bundling
+			additional = "--no-bundle-external -x {deps}".format(deps = " -x ".join(deps))
+			# by specifying certain external dependencies, we still bundle the
+			# custom packages that we've created.
+		else:
+			additional = "--no-bundle-external"
 	else: # bundle all external dependencies
-		in_stream = ' '.join(map(lambda s: "-r " + s, deps))
+		if deps: # only bundle dependencies, and nothing else
+			in_stream = "-r {deps}".format(deps = " -r ".join(deps))
+		else:
+			in_stream = ""
 		additional = ""
 
 	command = "browserify {in_path} -t [ reactify --everything --es6 ] -o {out_path} {additional}".format(
@@ -51,15 +74,24 @@ def browserify(in_path, out_path, deps):
 
 @task(name = "build-sass")
 def build_sass(external = False):
-	'''Compile SASS files'''
-	static_path = os.path.join(os.getcwd(), "static")
-	sass_path = lambda *paths: os.path.join(static_path, "sass", *paths)
-	css_path = lambda *paths: os.path.join(static_path, "css", *paths)
+	'''Compile SASS files
 
+	Arguments
+		bool external - whether or not to (separately) compile external dependencies
+	'''
+	
+	sass_to_css("custom-styles.scss", "custom-styles.css")
 	if external:
-		run("scss --style compressed {sass_path} {css_path}".format(
-			sass_path = sass_path("bootstrap.scss"),
-			css_path = css_path("bootstrap.css")))
+		sass_to_css("bootstrap.scss", "bootstrap.css")
+
+def sass_to_css(input_path, output_path):
+	'''Compile a SASS file to a CSS file
+
+	Arguments
+		str input_path - source input (SCSS) of file to compile
+		str output_path - output path of compiled CSS
+	'''
 	run("scss --style compressed {sass_path} {css_path}".format(
-		sass_path = sass_path("custom-styles.scss"),
-		css_path = css_path("custom-styles.css")))
+		sass_path = os.path.join(STATIC_PATH, "sass", input_path),
+		css_path = os.path.join(STATIC_PATH, "css", output_path)
+		))
