dfd8e4cbea45abbb68dc6a5dc425ccf2a7423dd2
yuanagain <yuanw@princeton.edu>
2016-02-03 23:52:53 -0500

    removed problem dir

--- bin_packer/dataUtils.py
+++
@@ -1,9 +0,0 @@
-import taskSequence as TS
-
-def print_instr(instructions, verbose = False):
-	"""
-	Prints the instructions
-	"""
-	print("%16s %10s %10s" % ("Name", "Start", "End"))
-	for task in instructions:
-		print("%16s %10s %10s" % (task["name"], task["start"], task["end"]))
