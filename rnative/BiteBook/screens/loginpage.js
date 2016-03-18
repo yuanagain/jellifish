6130f4b6952ad3666caff2845ac2ae5fa53890d3
yuanagain <yuanw@princeton.edu>
2016-03-18 15:47:31 -0400

    mocked up login screen mostly completed, lacks transitions and logo. At screens/loginpage.js

--- rnative/BiteBook/screens/loginpage.js
+++ rnative/BiteBook/screens/loginpage.js
@@ -2,8 +2,6 @@
 
 var React = require('react-native');
 var Dimensions = require('Dimensions');
-
-//var Button_Native = require('react-native-button');
 var windowSize = Dimensions.get('window');
 var Button = require('react-native-button');
 
@@ -35,23 +33,33 @@ var LoginPage = React.createClass({
     <View style={styles.container}>
 
       <View style={styles.header_container}>
-
+        <Image src={require('../assets/mtn.png')} style={styles.backgroundImage} />
+        <Text style={styles.title_text}>
+          ChefNotes
+        </Text>
       </View>
 
       <View style={styles.inputs_container}>
 
         <TextInput
         style={styles.email_input}
+
         onChangeText={(username) => this.setState({username})}
-        value={this.state.text}
+
+        value={this.state.username}
         placeholder={"user@email.com"}
         autoCapitalize={"none"}
         />
 
+        <View style={styles.white_line}>
+        </View>
+
         <TextInput
         style={styles.email_input}
+
         onChangeText={(password) => this.setState({password})}
-        value={this.state.text}
+
+        value={this.state.password}
         placeholder={"Password"}
         secureTextEntry={true}
         autoCapitalize={"none"}
@@ -61,97 +69,114 @@ var LoginPage = React.createClass({
       <View style={styles.buttons_container}>
         <Button
           style={styles.login_button}
-          styleDisabled={{color: 'red'}}
+          styleDisabled={{color: 'grey'}}
           onPress={this._handlePress}
           >
-          Login
+          Sign In
+        </Button>
+        <Button
+          style={styles.signup_button}
+          styleDisabled={{color: 'grey'}}
+          onPress={this._handlePress}
+          >
+          New user? Sign Up!
         </Button>
       </View>
-      <View style={styles.signup_container}>
-      </View>
+
     </View>
     );
-  },
-  animate: function() {
-    if (this.state.not_paused) {
-      var progress = this.props.progress
-      this.setState({ progress });
-      setTimeout(() => {
-        this.setState({ indeterminate: false });
-        setInterval(() => {
-          progress += 1/600;
-          if(progress > 1) {
-            progress = 1;
-          }
-          this.setState({ progress });
-        }, 100);
-      }, 100);
-    }
-  },
-
-  componentDidMount: function() {
-    this.animate();
-  },
-
-  _handlePress_toRecord(event) {
-    this.setState({show_screen: "recordscreen"})
   },
 });
 
 var styles = StyleSheet.create({
+  title_text: {
+    color: 'white',
+    fontSize: 50,
+    fontFamily: 'chalkduster',
+    padding: 24
+  },
   login_button: {
     color: 'white',
-    height: windowSize.height * 1 / 10,
-    width: windowSize.width,
+    //height: windowSize.height * 1 / 10,
+    //width: windowSize.width,
     alignItems: 'center',
     justifyContent: 'center',
-    flex: 0,
-    flexDirection: 'column',
+    flex: 1,
     fontSize: 40,
     textAlign: 'center',
+    backgroundColor: '#395E85',
+    width: windowSize.width,
+    padding: 15,
+    fontFamily: 'chalkduster',
+    shadowRadius: 4,
+    shadowColor: 'black',
+    shadowOpacity: 0.5,
+    shadowOffset: {width: 0, height: 3}
+  },
+  signup_button: {
+    fontSize: 20,
+    opacity: 1,
+    color: 'white',
+    padding: 5,
+    margin: 10,
+    fontFamily: 'avenir',
   },
   email_input: {
     height: 40,
-    borderColor: 'gray',
     borderWidth: 0,
-    fontSize: 40,
+    fontSize: 34,
     textShadowColor: 'white',
     color: 'white',
-    margin: 10
+    margin: 15,
+    marginVertical: 18,
+    fontFamily: 'avenir',
   },
   container: {
     flexDirection: 'column',
     flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
     backgroundColor: '#46D5B5',
     opacity: 0.92,
     margin: 0,
   },
   header_container: {
-    height: windowSize.height * 6 / 10,
+    // height: windowSize.height * 6 / 10,
     width: windowSize.width,
     alignItems: 'center',
     justifyContent: 'center'
   },
   inputs_container: {
     width: windowSize.width,
-    height: windowSize.height * 2 / 10,
+    //height: windowSize.height * 2 / 10,
     alignItems: 'center',
-    justifyContent: 'flex-start',
-    backgroundColor: 'white',
-    opacity: 0.32,
+    justifyContent: 'center',
+    backgroundColor: '#83e2cd',
+    opacity: 1.0,
   },
   buttons_container: {
-    height: windowSize.height * 1 / 10,
-    width: windowSize.width,
-    backgroundColor: '#395E85',
-  },
-  signup_container: {
-    height: windowSize.height * 1 / 10,
+    //height: windowSize.height * 1 / 10,
     width: windowSize.width,
     alignItems: 'center',
     justifyContent: 'center',
+    flex: 0,
+    backgroundColor: '#83e2cd',
+    // shadowRadius: 4,
+    // shadowColor: 'black',
+    // shadowOpacity: 0.5,
+    // shadowOffset: {width: 0, height: 0}
+  },
+  white_line: {
     backgroundColor: 'white',
-    opacity: 0.32,
+    height: 2,
+    opacity: 0.3,
+    width: windowSize.width
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', // or 'stretch'
+    width:320,
+    height:480,
   }
 })
 
