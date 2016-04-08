90c98da064eb1707160c517c201dc17ea21f44fa
ilyakrasnovsky <ilyakrasnovsky@gmail.com>
2016-04-08 16:03:05 -0400

    added fetchFBing (ingredient fetcher function from firebase) to rnative/bitebook/modules. firebasefetcher.js and testfirebasefetcher.html are just standalone testing materials

--- rnative/BiteBook/modules/datafetcher.js
+++ rnative/BiteBook/modules/datafetcher.js
@@ -45,6 +45,21 @@ function getOptimized(recipes, callback) {
 		}).then((res) => res.json()).then(callback);
 	}
 
+function fetchFBing(recipename, callback)
+{
+	// Get a database reference to our posts
+	var ref = new Firebase("https://jellifish.firebaseio.com/");
+	// Attach an asynchronous callback to read the data at our posts reference
+	ref.child(recipename).on("value", function(snapshot)
+	{
+	    callback(snapshot.val());
+	},
+	function (errorObject) 
+	{
+	  alert("The read failed: " + errorObject.code);
+	});
+}
+
 module.exports = {
 	getRecipes: getRecipes,
 	getOptimized: getOptimized
